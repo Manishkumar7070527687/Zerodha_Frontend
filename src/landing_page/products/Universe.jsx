5d2043f764d949394c5556390c07dde3a8210f07
Manish Kumar <manishgupta3859@gmail.com>
2025-11-13 12:26:52 +0530

    add sign up link

--- src/landing_page/products/Universe.jsx
+++ src/landing_page/products/Universe.jsx
@@ -96,7 +96,7 @@ function Universe() {
         </div>
       </div>
       <div className="text-center " style={{margin:"30px", marginBottom:"50px", }}>
-         <Link to={"/signup"}><button className=" fs-4 text-white rounded " style={{backgroundColor:"#387ED1" ,border: "1px solid  #387ED1"  }}>
+         <Link to={"/product/signup"}><button className=" fs-4 text-white rounded " style={{backgroundColor:"#387ED1" ,border: "1px solid  #387ED1"  }}>
           Sign up for free
         </button></Link>
       </div>
